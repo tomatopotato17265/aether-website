5f2e118ceabb556230b84f95bb49e24257505e88
tomato potato <nimaigos108@gmail.com>
2026-08-25 20:57:54 -0700

    fix: actually load embed image (hopefully)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://aether-website-2uc.pages.dev',
 	integrations: [
 		starlight({
 			title: 'Aether',
