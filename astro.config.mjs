f8e0aeca759554869a62a78ecc9194f0a0ae272a
tomato potato <nimaigos108@gmail.com>
2026-08-30 17:03:48 -0700

    fix: update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://aether-website-2uc.pages.dev',
+	site: 'https://aether.wback.workers.dev',
 	integrations: [
 		svelte(),
 		starlight({
